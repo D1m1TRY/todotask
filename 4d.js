0de159d38369c1cf6c691060e902100b11839ab4
D1m1TRY <rarikx@gmail.com>
2018-08-21 19:39:16 +0300

    initial commit

---
+++ 4d.js
@@ -0,0 +1,15 @@
+function viewDiv(){
+  document.getElementById("cd").style.display = "block";
+  document.getElementById("cd1").style.display = "none";
+  document.getElementById("cd2").style.display = "none";
+};
+function viewDiv1(){
+  document.getElementById("cd1").style.display = "block";
+  document.getElementById("cd").style.display = "none";
+  document.getElementById("cd2").style.display = "none";
+};
+function viewDiv2(){
+  document.getElementById("cd2").style.display = "block";
+    document.getElementById("cd1").style.display = "none";
+    document.getElementById("cd").style.display = "none";
+};
